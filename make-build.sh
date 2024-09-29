6a0117573f9faa4f4b282b24623b03837491843f
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-09-29 13:40:10 +0700

    Update make-build.sh

--- make-build.sh
+++ make-build.sh
@@ -58,7 +58,7 @@ PACKAGES+=" librespeed-go python3-speedtest-cli iperf3 luci-app-netspeedtest"
 PACKAGES+=" luci-app-base64"
 
 # tools mod
- PACKAGES+=" luci-app-tailscale mihomo luci-app-mihomo luci-app-droidnet luci-app-ipinfo luci-theme-initials luci-theme-hj atinout"
+ PACKAGES+=" luci-app-tailscale mihomo luci-app-mihomo luci-app-droidnet luci-app-ipinfo luci-theme-initials atinout"
 
 # Argon Theme
 PACKAGES+=" luci-theme-argon luci-app-argon-config"
