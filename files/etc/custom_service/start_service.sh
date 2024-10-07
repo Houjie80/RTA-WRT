515da7d2a62700b38035f06d38a01595a1bcb170
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-10-07 20:24:18 +0700

    Add files via upload

--- files/etc/custom_service/start_service.sh
+++ files/etc/custom_service/start_service.sh
@@ -13,37 +13,6 @@
 # File path: /etc/custom_service/start_service.sh
 #
 #========================================================================================
-#
-# Find the partition where root is located
-ROOT_PTNAME="$(df -h /boot | tail -n1 | awk '{print $1}' | awk -F '/' '{print $3}')"
-if [[ -n "${ROOT_PTNAME}" ]]; then
-
-    # Find the disk where the partition is located, only supports mmcblk?p? sd?? hd?? vd?? and other formats
-    case "${ROOT_PTNAME}" in
-    mmcblk?p[1-4])
-        DISK_NAME="${ROOT_PTNAME:0:-2}"
-        PARTITION_NAME="p"
-        ;;
-    [hsv]d[a-z][1-4])
-        DISK_NAME="${ROOT_PTNAME:0:-1}"
-        PARTITION_NAME=""
-        ;;
-    nvme?n?p[1-4])
-        DISK_NAME="${ROOT_PTNAME:0:-2}"
-        PARTITION_NAME="p"
-        ;;
-    *)
-        DISK_NAME=""
-        PARTITION_NAME=""
-        ;;
-    esac
-
-    PARTITION_PATH="/mnt/${DISK_NAME}${PARTITION_NAME}4"
-else
-    PARTITION_PATH=""
-fi
-#
-#========================================================================================
 
 # Custom Service Log
 custom_log="/tmp/ophub_start_service.log"
@@ -55,45 +24,6 @@ echo "[$(date +"%Y.%m.%d.%H:%M:%S")] Start the custom service..." >${custom_log}
 [[ -x "/usr/sbin/balethirq.pl" ]] && {
     perl /usr/sbin/balethirq.pl 2>/dev/null &&
         echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The network optimization service started successfully." >>${custom_log}
-}
-
-# Led display control
-openvfd_enable="no"
-openvfd_boxid="15"
-[[ "${openvfd_enable}" == "yes" && -n "${openvfd_boxid}" && -x "/usr/sbin/openwrt-openvfd" ]] && {
-    openwrt-openvfd ${openvfd_boxid} &&
-        echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The openvfd service started successfully." >>${custom_log}
-}
-
-# For vplus(Allwinner h6) led color lights
-[[ -x "/usr/bin/rgb-vplus" ]] && {
-    rgb-vplus --RedName=RED --GreenName=GREEN --BlueName=BLUE &
-    echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The LED of Vplus is enabled successfully." >>${custom_log}
-}
-
-# For fan control service
-[[ -x "/usr/bin/pwm-fan.pl" ]] && {
-    perl /usr/bin/pwm-fan.pl 2>/dev/null &
-    echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The fan control service enabled successfully." >>${custom_log}
-}
-
-# Automatic expansion of the third and fourth partitions
-todo_rootfs_resize="/root/.todo_rootfs_resize"
-[[ -f "${todo_rootfs_resize}" && "$(cat ${todo_rootfs_resize} 2>/dev/null | xargs)" == "yes" ]] && {
-    openwrt-tf 2>/dev/null &&
-        echo "[$(date +"%Y.%m.%d.%H:%M:%S")] Automatically expand the partition successfully." >>${custom_log}
-}
-
-# Set swap check file
-sawp_check_file="${PARTITION_PATH}/.swap/swapfile"
-[[ -f "${sawp_check_file}" ]] && {
-    # Set swap space
-    swap_loopdev="$(losetup -f)"
-    # Mount swap file
-    losetup ${swap_loopdev} ${sawp_check_file}
-    # Enable swap
-    swapon ${swap_loopdev}
-    [[ "${?}" == 0 ]] && echo "[$(date +"%Y.%m.%d.%H:%M:%S")] The swap file enabled successfully." >>${custom_log}
 }
 
 # Add custom log
